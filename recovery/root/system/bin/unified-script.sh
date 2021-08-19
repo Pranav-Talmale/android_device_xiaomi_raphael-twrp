de593c90e8db1712e55a751eeed210ca81154d54
Darth9 <reyprimerjefe@gmail.com>
2021-08-19 22:59:13 +0100

    Activate the unified script

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -3,24 +3,24 @@
 
 load_vayu()
 {
-    resetprop "ro.product.model" "Poco X3 Pro NFC"
     resetprop "ro.product.name" "vayu"
     resetprop "ro.build.product" "vayu"
     resetprop "ro.product.device" "vayu"
     resetprop "ro.product.system.device" "vayu"
     resetprop "ro.product.vendor.device" "vayu"
     resetprop "ro.vendor.product.device" "vayu"
+    resetprop "ro.product.model" "Poco X3 Pro NFC (M2102J20SG)"
 }
 
 load_bhima()
 {
-    resetprop "ro.product.model" "Poco X3 Pro"
     resetprop "ro.product.name" "bhima"
     resetprop "ro.build.product" "bhima"
     resetprop "ro.product.device" "bhima"
     resetprop "ro.product.system.device" "bhima"
     resetprop "ro.product.vendor.device" "bhima"
     resetprop "ro.vendor.product.device" "bhima"
+    resetprop "ro.product.model" "Poco X3 Pro (M2102J20SG)"
 }
 
 variant=$(getprop ro.boot.hwc)
