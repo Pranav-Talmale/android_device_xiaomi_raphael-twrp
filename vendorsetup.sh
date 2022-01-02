04fe1867b3ee0024d1e3bfc869efaa086f666414
Darth9 <reyprimerjefe@gmail.com>
2022-01-02 21:40:11 +0000

    Upgrade the magisk addon to v23.0
    Change-Id: I008554658c7efe79850cd36728b133826bcee8a4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -55,7 +55,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_PATCH_AVB20=1
         export FOX_DELETE_AROMAFM=1
         export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-        export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+        export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-23.0.zip
 
         # OTA
         export OF_KEEP_DM_VERITY=1
