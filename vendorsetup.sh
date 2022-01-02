4fdbcfde5690bb3608701daf07aa0581f0b26e54
Darth9 <reyprimerjefe@gmail.com>
2022-01-02 21:49:32 +0000

    Update copyright notice
    Change-Id: I8f97772260c6e993b2f0cecbe07f3bee8988b380

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021 The OrangeFox Recovery Project
+# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
