d5ce77a85b51c76dc8beeca751f32cbce5240f6f
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-06-23 17:52:17 +0530

    add option to format /data in f2fs format

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 # File systems
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
