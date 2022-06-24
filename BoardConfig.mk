64e96511bd2edd3489a7c305e989226a32a07568
Pranav-Talmale <67180305+Pranav-Talmale@users.noreply.github.com>
2022-06-24 08:45:47 +0530

    ALLOW_MISSING_DEPENDENCIES := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,9 @@
 # limitations under the License.
 #
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
